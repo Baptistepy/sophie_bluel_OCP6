0e3cd190ee4a14264d60b083e7ddc2e0d7bc3117
Baptiste Boucly <b.boucly@outlook.com>
2023-11-29 17:57:25 +0100

    data from backend recover with fetch

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,2 +1,39 @@
 "use strict";
 
+// ******************************* CONSTANTES *******************************
+
+const apiUrl = 'http://localhost:5678/api/works';
+
+const figures = document.querySelectorAll('figure');
+
+// ******************************* VARIABLES *******************************
+// ******************************* FONCTIONS *******************************
+
+
+// ******************************* CODE PRINCIPAL *******************************
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(project => {
+            const figure = document.createElement('figure');
+
+            const img = document.createElement('img');
+
+            img.src = project.imageUrl; 
+            img.alt = project.imageAlt; 
+
+            const figcaption = document.createElement('figcaption');
+            figcaption.textContent = project.caption; 
+
+            figure.appendChild(img);
+
+            figure.appendChild(figcaption);
+
+            const parentElement = document.querySelector('.gallery');
+            parentElement.appendChild(figure);
+        });
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite:', error);
+    });
