e6b872a9b447df8388d52eb3aa9df51644514cd4
Baptiste Boucly <b.boucly@outlook.com>
2023-11-30 13:10:49 +0100

    change on fetch function and creation of filter function

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -2,35 +2,85 @@
 
 // ******************************* CONSTANTES *******************************
 
-const apiUrl = 'http://localhost:5678/api/works';
+const API_URL = 'http://localhost:5678/api';
 
-const figures = document.querySelectorAll('figure');
+const gallery = document.querySelector('.gallery');
 
 // ******************************* VARIABLES *******************************
+
+let works = [];
+let categories = [];
+
 // ******************************* FONCTIONS *******************************
+
+/**
+ * Creates a new element and appends it to the gallery.
+ *
+ * @param {Object} work - The work object containing the necessary information.
+ * @param {string} work.imageUrl - The URL of the image to be displayed.
+ * @param {string} work.imageAlt - The alternate text for the image.
+ * @param {string} work.caption - The caption for the image.
+ */
+function createWork(work) {
+  const figure      = document.createElement('figure');
+  const img         = document.createElement('img');
+  const figcaption  = document.createElement('figcaption');
+
+  img.src                 = work.imageUrl;
+  img.alt                 = work.imageAlt;
+  figcaption.textContent  = work.caption;
+
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
+
+/**
+ * Creates all works.
+ */
+function createAllWorks() {
+  works.forEach(work => { createWork(work) });
+}
+
+/**
+ * Retrieves elements from the API and creates an element for each project.
+ */
+async function getWorks() {
+  try {
+    const response = await fetch(API_URL + '/works');
+    works = await response.json();
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getCategories() {
+  try {
+    const response = await fetch(API_URL + '/categories');
+    categories = await response.json();
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function filterElements () {
+  categories = await getCategories();
+  const btns = document.querySelectorAll('.btn');
+  console.log(btns)
+  console.log(works)
+  console.log(categories)
+}
 
 
 // ******************************* CODE PRINCIPAL *******************************
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(project => {
-            const figure = document.createElement('figure');
-
-            const img = document.createElement('img');
-
-            img.src = project.imageUrl; 
-            img.alt = project.imageAlt; 
-
-            const figcaption = document.createElement('figcaption');
-            figcaption.textContent = project.caption; 
-
-            figure.appendChild(img);
-
-            figure.appendChild(figcaption);
-
-            const parentElement = document.querySelector('.gallery');
-            parentElement.appendChild(figure);
-        });
-    })
+getWorks()
+  .then(() => {
+    createAllWorks();
+    filterElements();
+  }) 
+  .catch(error => {
+    console.error(error);
+  })
