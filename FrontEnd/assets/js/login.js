33b3393223f84e40c50d3d3379d7ccbcbc3d2701
Baptiste Boucly <b.boucly@outlook.com>
2023-12-14 15:00:30 +0100

    login working

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,54 +1,47 @@
 "use strict";
 
 // ******************************* CONSTANTES *******************************
+
 const emailElt = document.getElementById('email');
 const passwordElt = document.getElementById('password');
 
-const email = emailElt.value;
-const password = passwordElt.value;
-
-const user = {
-  email: email,
-  password: password
+const data = {
+  "email": "sophie.bluel@test.tld",
+  "password": "S0phie"
 };
 
-const URL = "http://localhost:5678/api/users/login";
+const LOGIN_URL = 'http://localhost:5678/api/users/login';
 
 // ******************************* VARIABLES *******************************
 
 
 // ******************************* FONCTIONS *******************************
 
-    function loginUser(event) {
+function loginUser(event) {
+  event.preventDefault();
+  fetch(LOGIN_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.userId && data.token) {
+        const token = data.token;
+        const userId = data.userId;
 
-        event.preventDefault();
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', userId);
 
-        fetch(URL, { 
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.userId && data.token) {
-            const token = data.token;
-            const userId = data.userId;
-    
-            localStorage.setItem('token', token);
-            localStorage.setItem('userId', userId);
-    
-            window.location.href = 'index.html';
-          } else {
-            alert("Erreur dans l'identifiant ou le mot de passe.");
-          }
-        })
-        .catch(error => {
-            console.error("Une erreur s'est produite lors de la requête:", error);
-        });
-    }
+        window.location.href = 'index.html';
+      } else {
+        alert("Erreur dans l'identifiant ou le mot de passe.");
+      }
+    })
+    .catch(error => {
+      console.error("Une erreur s'est produite lors de la requête:", error);
+    });
+}
 
 
     
@@ -57,4 +50,3 @@ const URL = "http://localhost:5678/api/users/login";
 document.getElementById('login').addEventListener('submit', function (event) {
   loginUser(event);
 });
-console.log(user);
